569a78381002bd6ad639be85fbaccf9550f7a9cd
yves848 <yves.godart@gmail.com>
2025-02-24 18:05:52 +0100

    VIP

--- main.ps1
+++ main.ps1
@@ -1,6 +1,7 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 . ./dependencies.ps1
+. ./visuals.ps1
 
 [System.Console]::Clear()
 if ($Global:scoop) {
@@ -11,4 +12,6 @@ if ($Global:scoop) {
 Write-SpectreFigletText -Text "Packaroo" -FigletFontPath ".\3d.flf" -Alignment Center -PassThru |  Format-SpectrePanel  -title $title -Border "Rounded" -Color "Magenta1" -Expand
 
 # Write-SpectreRule -Alignment Center -Color Yellow
-Format-SpectrePanel -Border "Rounded" -Expand -Height 20
+Format-SpectrePanel -Border "Rounded" -Expand -Height 20 -Data ""
+
+$BoxChars
---
+++ visuals.ps1
@@ -0,0 +1,34 @@
+# Définition d'une table de caractères pour le dessin de cadres
+$BoxChars = @{
+    "HLine"  = "─"  # Ligne horizontale simple
+    "VLine"  = "│"  # Ligne verticale simple
+    "TopLeft" = "╭" # Coin supérieur gauche arrondi
+    "TopRight" = "╮" # Coin supérieur droit arrondi
+    "BottomLeft" = "╰" # Coin inférieur gauche arrondi
+    "BottomRight" = "╯" # Coin inférieur droit arrondi
+
+    # Lignes doubles (utile pour encadrements plus marqués)
+    "HLineDouble" = "═"
+    "VLineDouble" = "║"
+    "TopLeftDouble" = "╔"
+    "TopRightDouble" = "╗"
+    "BottomLeftDouble" = "╚"
+    "BottomRightDouble" = "╝"
+
+    # Connexions simples
+    "TLeft" = "├"   # T vers la gauche
+    "TRight" = "┤"  # T vers la droite
+    "TTop" = "┬"    # T vers le haut
+    "TBottom" = "┴" # T vers le bas
+    "Cross" = "┼"   # Croisement en plus
+
+    # Connexions doubles
+    "TLeftDouble" = "╠"
+    "TRightDouble" = "╣"
+    "TTopDouble" = "╦"
+    "TBottomDouble" = "╩"
+    "CrossDouble" = "╬"
+}
+
+# Affichage de la table
+$BoxChars
