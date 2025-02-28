1f56ebc48fe269d19de67720ccfea44d3ca1fa84
yves848 <yves.godart@gmail.com>
2025-02-28 15:17:34 +0100

    WIP

--- packaroo.ps1
+++ packaroo.ps1
@@ -7,11 +7,34 @@ Import-Module PwshSpectreConsole
 Import-Module Microsoft.WinGet.Client
 
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$columns = New-Object 'System.Collections.Generic.List[PSCustomObject]'
 
 $packs = Get-WinGetPackage | where-Object {$_.Source -eq "Winget"}
 
 . ./dependencies.ps1
 . ./visuals.ps1
+function buildGrid {
+  $ratio = [Math]::Round($width/128,3)
+  $columns.Clear()
+  $c0 = 4
+  $c1 = [Math]::Round(52*$ratio)
+  $c2 = [Math]::Round(34*$ratio)
+  $c3 = [Math]::Round(22*$ratio)
+  $c4 = 10
+  $total = $c0+$c1+$c2+$c3+$c4+6
+  if ($total -lt $width) {
+    $c1_1 = [Math]::Round(($width - $total)/3*2)
+    $c2_1 = ($width - $total) -$c1_1
+  }
+  $c1 += $c1_1
+  $c2 += $c2_1
+  
+}
+
+
+
+
+
 
 [System.Console]::Clear()
 if ($Global:scoop) {
@@ -38,9 +61,6 @@ if ($Global:scoop) {
 # [system.Console]::ForegroundColor = "Blue"
 # [system.Console]::WriteLine("$c3")
 
-
+buildGrid
 # $global:Host.UI.RawUI.ReadKey() | Out-Null
 
-function buildGrid() {
-  
-}
