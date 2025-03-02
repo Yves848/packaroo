a87e8af623d93b2415447d6cbaf9008801a96ab7
yves848 <yves.godart@gmail.com>
2025-03-02 14:21:10 +0100

    >IP

--- packaroo.ps1
+++ packaroo.ps1
@@ -136,22 +136,6 @@ if ($Global:scoop) {
 # Write-SpectreFigletText -Text "Packaroo" -FigletFontPath ".\3d.flf" -Alignment Center -PassThru |  Format-SpectrePanel  -title $title -Border "Rounded" -Color "Magenta1" -Expand 
 # $h2 = $height - [console]::GetCursorPosition().Item2 -2
 
-# $c1 = "Test".PadLeft(20," ").PadRight(20," ")
-# $c2 = ""
-# $c3 = ""
-# [system.console]::SetCursorPosition(10,25)
-# [System.Console]::Write("$width $height $h2")
-
-# [System.Console]::BackgroundColor = "Black"
-# [system.Console]::ForegroundColor = "Blue"
-# [system.console]::Write("$c2")
-# [System.Console]::BackgroundColor = 'Blue'
-# [system.Console]::ForegroundColor = "Red"
-# [System.Console]::Write($c1)
-# [System.Console]::BackgroundColor = "Black"
-# [system.Console]::ForegroundColor = "Blue"
-# [system.Console]::WriteLine("$c3")
-# [console]::WriteLine("Width : {$width} Height : {$height}")
 [system.console]::SetCursorPosition(0,0)
 $header = builHeader
 $grid = buildGrid
