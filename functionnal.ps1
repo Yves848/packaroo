27fdcb602659499815c86bf5edc0c4d7e6f11117
yves848 <yves.godart@gmail.com>
2025-03-03 09:39:53 +0100

    scoop in another session

--- functionnal.ps1
+++ functionnal.ps1
@@ -1,29 +1,46 @@
 . ./dependencies.ps1
 
 function buildPackages {
-  $packs = Get-WinGetPackage | where-Object {$_.Source -eq "Winget"}
+  $packs = Get-WinGetPackage | Where-Object { $_.Source -eq "Winget" }
   $lines = New-Object 'System.Collections.Generic.List[Pack]'
   $packs | ForEach-Object {
-    [Pack]$pack = [Pack]::new($_.Name,$_.Id,$_.InstalledVersion,$_.Source,"")
+    [Pack]$pack = [Pack]::new($_.Name, $_.Id, $_.InstalledVersion, $_.Source, "")
     $lines.Add($pack)
   }
-  $packs = Invoke-Expression -command "scoop list" | Out-Null
+  <#
+    Collecting scoop data mut be done in another session to avoid screen output.
+  #>
+  $statedata = [System.Collections.Hashtable]::Synchronized([System.Collections.Hashtable]::new())
+  $runspace = [runspacefactory]::CreateRunspace()
+  $statedata.packs = $packs
+  $runspace.Open()
+  $Runspace.SessionStateProxy.SetVariable("StateData", $this.StateData)
+  $sb = {
+    $Statedate.packs = Invoke-Expression -Command "scoop list" | Out-Null    
+  }
+  $session = [powershell]::create()
+  $null = $session.AddScript($sb)
+  $session.Runspace = $this.runspace
+  $null = $session.BeginInvoke()
+  $session.Stop()
+  $runspace.Close()
+  $runspace.Dispose()
   $packs | ForEach-Object {
-    [Pack]$pack = [Pack]::new($_.Name,$_.Name,$_.version,"Scoop",$_.source)
+    [Pack]$pack = [Pack]::new($_.Name, $_.Name, $_.version, "Scoop", $_.source)
     $lines.Add($pack)
   }
   return $lines
 }
 
-function displayPackages{
+function displayPackages {
   param(
     [System.Collections.Generic.List[Pack]]$list
   )
 
   $skip = 0;
   $selected = 0;
-  while($true) {
-    $visible = $list | Select-Object -skip 0 -First $script:gh
+  while ($true) {
+    $visible = $list | Select-Object -Skip 0 -First $script:gh
     if ([console]::KeyAvailable) {
       [System.Management.Automation.Host.KeyInfo]$key = $($global:host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'))
       if ($key.VirtualKeyCode -eq 27) {
