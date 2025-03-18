730b12cea1faea2c804daebfd6e713be4577a6b2
yves848 <yves.godart@gmail.com>
2025-03-18 16:59:54 +0100

    wip

--- functionnal.ps1
+++ functionnal.ps1
@@ -137,6 +137,7 @@ function drawList {
     $i++
   }
 }
+
 function drawPopup {
   $popup = "test" | Format-SpectrePadded -Padding 1 | Format-SpectrePanel -Width 25 -Header "Essai" -Border "Rounded" -Color "SlateBlue3" | Out-SpectreHost
   [System.Console]::SetCursorPosition(20,20)
