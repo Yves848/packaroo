fd776e2a66e9bfd84134b0c21c953289498cad23
yves848 <yves.godart@gmail.com>
2025-03-19 17:04:01 +0100

    wip

--- packaroo.ps1
+++ packaroo.ps1
@@ -154,10 +154,10 @@ $header = builHeader
 $script:grid = buildGrid
 $footer =  buildFooter
 Write-Host "$header$grid$Footer" -NoNewline
-# $Spinner = [Spinner]::new("Dots",3,$height-2)
-# $Spinner.Start("Loading Packages List")
+$Spinner = [Spinner]::new("Dots",3,$height-2)
+$Spinner.Start("Loading Packages List")
 $Script:list = buildPackages
-# $Spinner.Stop()
+$Spinner.Stop()
 [system.console]::SetCursorPosition(0,0)
 [Console]::Write("$header$grid$Footer")
 Start-Sleep -Seconds 3
